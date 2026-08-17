f76ba1fa81f108b067f4f69e6c0fdea5352e3dcd
Leonardo Souza <leonsouza.contato@gmail.com>
2026-08-17 13:36:20 -0300

    Update and rename CalculadoraPHP.txt to Calculadora.php

--- Calculadora.php
+++ Calculadora.php
@@ -31,7 +31,7 @@ switch ($op) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora PHP</title>
+    <title>Resultado da Calculadora</title>
 </head>
 <body>
     <?php echo "<h1>Resultado: $result</h1>"; ?>
